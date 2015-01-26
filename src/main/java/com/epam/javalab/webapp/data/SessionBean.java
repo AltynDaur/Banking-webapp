aa35e394d6692f50460533fb553f7df3ff2e5317
Dauren <altyndaur@gmail.com>
2015-01-26 17:00:16 +0600

    After apocalypse part 2

---
+++ src/main/java/com/epam/javalab/webapp/data/SessionBean.java
@@ -0,0 +1,79 @@
+package com.epam.javalab.webapp.data;
+
+import com.epam.javalab.webapp.user.User;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateful;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Locale;
+
+@Named
+@Stateful
+@SessionScoped
+public class SessionBean implements Serializable {
+
+    private Locale locale;
+
+    private User user;
+
+    @RequestScoped
+    private String repeatPassword;
+
+    @Inject
+    private List<String> languages;
+
+
+    @PostConstruct
+    public void init() {
+        locale = FacesContext.getCurrentInstance().getApplication().getDefaultLocale();
+        user = new User();
+    }
+
+    public List<String> getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(List<String> languages) {
+        this.languages = languages;
+    }
+
+    public String getLanguage() {
+        return locale.getLanguage();
+    }
+
+    public void setLanguage(String language) {
+        locale = new Locale(language);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
+    }
+
+    public void clear() {
+        setUser(null);
+        setLanguage(null);
+    }
+}
---
+++ src/main/java/com/epam/javalab/webapp/util/Resources.java
@@ -0,0 +1,45 @@
+package com.epam.javalab.webapp.util;
+
+import org.apache.log4j.Logger;
+
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.faces.context.FacesContext;
+import javax.inject.Singleton;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Resources implements Serializable {
+    @SuppressWarnings("unused")
+    @Produces
+    @PersistenceContext(unitName = "Banking")
+    private EntityManager em;
+
+    @Produces
+    public Logger getLogger(InjectionPoint injectionPoint) {
+        return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
+    }
+
+    @Produces
+
+    public FacesContext produceFacesContext() {
+        return FacesContext.getCurrentInstance();
+    }
+
+    @Produces
+    public List<String> languages() {
+        List<String> languages = new ArrayList<>();
+        languages.add("ru");
+        languages.add("en");
+        return languages;
+    }
+
+    ;
+
+}
